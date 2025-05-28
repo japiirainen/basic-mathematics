362cb2fbf0fd9c07e9093e87f276ea56015f5a19
japiirainen <joona.piirainen@gmail.com>
2025-05-28 11:12:46 +0200

    feat(numbers): add proof of N2

--- BasicMathematics/Algebra/Numbers.lean
+++ BasicMathematics/Algebra/Numbers.lean
@@ -1,12 +1,21 @@
+import Mathlib.Data.Int.Basic
+
 /-!
 ## 1. The Integers
 -/
 
 -- Theorem N1.
--- ∀ a : ℤ. 0 + a = a + 0 = a
+-- a) ∀ a : ℤ. 0 + a = a
+-- b) ∀ a : ℤ. a + 0 = a
 
-theorem n1a : ∀ (a : Int), 0 + a = a := by
-  simp
+theorem n1a : ∀ (a : Int), 0 + a = a := by simp
 
-theorem n1b : ∀ (a : Int), a + 0 = a := by
-  simp
+theorem n1b : ∀ (a : Int), a + 0 = a := by simp
+
+-- Theorem N2.
+-- a) ∀ a : ℤ. a + (-a) = 0
+-- b) ∀ a : ℤ. -a + a = 0
+
+theorem n2a : ∀ (a : Int), a + (-a) = 0 := Int.add_right_neg
+
+theorem n2b : ∀ (a : Int), -a + a = 0 := Int.add_left_neg
