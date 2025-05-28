982d275509b390d8c46e6dba0b3aa038571f8385
japiirainen <joona.piirainen@gmail.com>
2025-05-28 10:57:54 +0200

    feat(numbers): prove theorem N1

--- BasicMathematics/Algebra/Numbers.lean
+++ BasicMathematics/Algebra/Numbers.lean
@@ -1,1 +1,12 @@
-def hello := "world"
+/-!
+## 1. The Integers
+-/
+
+-- Theorem N1.
+-- ∀ a : ℤ. 0 + a = a + 0 = a
+
+theorem n1a : ∀ (a : Int), 0 + a = a := by
+  simp
+
+theorem n1b : ∀ (a : Int), a + 0 = a := by
+  simp
